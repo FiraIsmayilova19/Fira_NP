0e1fe91a81dc0b8f8adef36fe345a401809c5a4d
FiraIsmayilova19 <ismayilovafira004@gmail.com>
2024-10-21 23:54:44 +0400

    Tcp2

---
+++ TCPTask2/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var port = 27001;
+
+var ep = new IPEndPoint(IPAddress.Loopback, 27001);
+
+var client = new TcpClient();
+try
+{
+    client.Connect(ep);
+    if (client.Connected)
+    {
+        var networkStream = client.GetStream();
+        Console.WriteLine("enter path: ");
+        var path = Console.ReadLine();
+        FileInfo file = new FileInfo(path);
+        var fileLengthBytes = Encoding.UTF8.GetBytes(file.Length.ToString());
+        networkStream.Write(fileLengthBytes, 0, fileLengthBytes.Length);
+        var fileNameBytes = Encoding.UTF8.GetBytes(file.Name);
+        networkStream.Write(fileNameBytes, 0, fileNameBytes.Length);
+
+
+        using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            var buffer = new byte[fileLengthBytes.Length];
+            var len = 0;
+
+
+            while ((len = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                networkStream.Write(buffer, 0, len);
+            }
+
+            networkStream.Close();
+            client.Close();
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ TCPTask2/TCPTask2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var port = 27001;
+var ep = new IPEndPoint(IPAddress.Loopback, 27001);
+
+var listener = new TcpListener(ep);
+
+try
+{
+    listener.Start();
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+
+        _ = Task.Run(() =>
+        {
+            var networkStream = client.GetStream();
+            var remoteEp = client.Client.RemoteEndPoint as IPEndPoint;
+            var directoryPath = Path.Combine(Environment.CurrentDirectory, remoteEp!.Address.ToString());
+
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+            var buffer = new byte[4];
+            var fileNameLength = networkStream.Read(buffer, 0, buffer.Length);
+            var fileNameBuffer = new byte[fileNameLength];
+            var fileNameBytes = networkStream.Read(fileNameBuffer, 0, fileNameLength);
+            var fileName = Encoding.UTF8.GetString(fileNameBuffer);
+
+
+            var path = Path.Combine(directoryPath, $"{DateTime.Now:dd.MM.yyyy.HH.mm.ss}.{Path.GetExtension(fileName)}");
+
+
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                int len = 0;
+                var bytes = new byte[fileNameLength];
+
+                while ((len = networkStream.Read(bytes, 0, bytes.Length)) > 0)
+                {
+                    fs.Write(bytes, 0, len);
+                }
+            };
+
+            Console.WriteLine("File recieved");
+            client.Close();
+        });
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+

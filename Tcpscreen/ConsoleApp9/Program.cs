3b359dc673fdb6b79af7eeb6f3b8a39e281ac4e3
FiraIsmayilova19 <ismayilovafira004@gmail.com>
2024-10-21 18:49:19 +0400

    .

---
+++ Tcpscreen/ConsoleApp9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+
+//var ip = IPAddress.Parse("192.168.1.76");
+var port = 27001;
+
+var ep = new IPEndPoint(IPAddress.Loopback, 27001);
+Console.ReadKey();
+try
+{
+    while (true)
+    {
+        _ = Task.Run(() => {
+            using(var client=new TcpClient())
+            {
+                client.Connect(ep);
+                if (client.Connected) 
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        Bitmap memoryImage;
+                        memoryImage = new Bitmap(1920, 1080);
+                        Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+                        Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+                        memoryImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        var buffer = ms.ToArray();
+                        var networkStream = client.GetStream();
+                        networkStream.Write(buffer, 0, buffer.Length);
+
+                        networkStream.Flush();
+                    }
+                    Console.WriteLine("gonderildi");
+                    client.Close();
+                }
+                else
+                {
+                    Console.WriteLine("qosulmaq olmadi");
+                }
+            }
+        });
+        await Task.Delay(10000);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ Tcpscreen/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+//var ip = IPAddress.Parse("192.168.1.76");
+var port = 27001;
+var ep = new IPEndPoint(IPAddress.Loopback, 27001);
+var listener = new TcpListener(ep);
+
+try
+{
+    listener.Start();
+    Console.WriteLine("Server başladı");
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+
+        if (client != null)
+        {
+
+            _ = Task.Run(() =>
+            {
+                var networkstream = client.GetStream();
+                var remoteEp = client.Client.RemoteEndPoint as IPEndPoint;
+
+
+                var directoryPath = Path.Combine(Environment.CurrentDirectory, remoteEp.Address.ToString());
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+                var path = Path.Combine(directoryPath, $"{DateTime.Now:dd.MM.yyyy.HH.mm.ss}.png");
+
+                using (var filestream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    int len = 0;
+                    var bytes = new byte[1024];
+
+
+                    while ((len = networkstream.Read(bytes, 0, bytes.Length)) > 0)
+                    {
+                        filestream.Write(bytes, 0, len);
+                    }
+                }
+            });
+        }
+        else
+        {
+            Console.WriteLine("Mushteri qebul edilmedi.");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
